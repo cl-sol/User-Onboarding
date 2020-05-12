f511b93a5a0b5ac6c6995429f24e2f8e5193387c
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-05-12 19:37:48 -0400

    finished step 1

--- adv-form-management/src/Components/Form.js
+++ adv-form-management/src/Components/Form.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
+import * as yup from "yup";
+import axios from "axios";
 
 const Form = () => {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
+    });
+
     return (
-        "Hello, World"
+        <form 
+            onSubmit = {e => {
+                e.preventDefault();
+        }}> 
+            <label htmlFor = "name">Name:
+                <input
+                    id = "name"
+                    type = "text"
+                    name = "name"
+                    placeholder = "Type your name here"
+                    //value = 
+                    //onChange =
+                />
+            </label>
+            <label htmlFor = "email">Email:
+                <input
+                    id = "email"
+                    type = "text"
+                    name = "email"
+                    placeholder = "Type your email here"
+                    //value = 
+                    //onChange =
+                />
+            </label>
+            <label htmlFor = "password">Password:
+                <input
+                    id = "password"
+                    type = "text"
+                    name = "password"
+                    placeholder = "Type your password here"
+                    //value = 
+                    //onChange =
+                />
+            </label>
+            <label htmlFor = "terms">
+                <input
+                    id = "terms"
+                    type = "checkbox"
+                    name = "terms"
+                    //checked = 
+                    //onChange = 
+                />
+            </label>
+            I agree to the Terms and Conditions
+            <button type = "submit"> Click here to submit!</button>
+        </form>
     )
 }
 
